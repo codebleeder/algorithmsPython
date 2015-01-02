f1f69eee68d9bb040fac9ec3b8661bb146022ea0
codebleeder <sharad.shivmath@gmail.com>
2015-01-01 17:46:04 -0800

    solution02SortAndCompare.py

---
+++ 01anagramDetection/solution02SortAndCompare.py
@@ -0,0 +1,20 @@
+__author__ = 'cloudera'
+
+def anagramSolution2(s1, s2):
+    list1 = list(s1)
+    list2 = list(s2)
+    list1.sort() # complexity will depend on these sort steps
+    list2.sort()
+    match = True
+
+    #compare w.r.t position
+    pos = 0
+    for pos in range(len(list1)):
+        if list1[pos] != list2[pos]:
+            match = False
+
+    return match
+
+#main
+print anagramSolution2('earth', 'heart')
+
